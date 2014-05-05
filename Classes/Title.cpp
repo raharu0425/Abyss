9dbd82b3ae3970153420d5326384f310d21097ed
raharu0425 <raharu0425@gmail.com>
2014-05-05 15:55:27 +0900

    websocketの一時取得

--- Classes/Title.cpp
+++ Classes/Title.cpp
@@ -1,6 +1,12 @@
 #include "Title.h"
 
-USING_NS_CC;
+//コンストラクタ
+Title::Title()
+{
+    if (websocket) websocket->close();
+}
+
+
 
 Scene* Title::createScene()
 {
@@ -18,21 +24,72 @@ bool Title::init()
         return false;
     }
     
+    //窓サイズを取得
+    window_size = Director::getInstance()->getWinSize();
+    
     //タイトルの読み込み
     this->crateTitle();
     
     //外部DBとの接続
-    //this->loadDataBase();
+    this->loadStory();
+    
     
     return true;
 }
+
+//ストーリーのロード
+void Title::loadStory()
+{
+    
+    // ステータスラベル作成
+    
+    status = LabelTTF::create("オープン中...", "Arial", 15);
+    status->setPosition(Point(window_size.width / 2 , window_size.height - 20));
+    this->addChild(status);
+    
+    // WebSocket生成
+    websocket = new WebSocket();
+    websocket->init(*this, "ws://raharu.net:8080/echo");
+}
+
+void Title::onOpen(WebSocket* ws)
+{
+    status->setString("オープンしました");
+    scheduleOnce(schedule_selector(Title::sendMessage), 1.0f);
+}
+
+void Title::onMessage(WebSocket* ws, const WebSocket::Data& data)
+{
+    String *text = String::createWithFormat("response msg: %s", data.bytes);
+    status->setString(text->getCString());
+}
+
+void Title::onClose(WebSocket* ws)
+{
+    websocket = nullptr;
+    status->setString("クローズしました");
+}
+
+void Title::onError(WebSocket* ws, const WebSocket::ErrorCode& error)
+{
+    char buf[100] = {0};
+    sprintf(buf, "エラーが発生しました code: %d", error);
+    status->setString(buf);
+}
+
+void Title::sendMessage(float dt)
+{
+    if (websocket->getReadyState() == WebSocket::State::OPEN){
+        status->setString("メッセージを送信しました");
+        websocket->send("Hello WebSocket,\nI'm a text message.");
+    }
+}
+
 
 
 //タイトルの作成
 void Title::crateTitle()
 {
-    //窓サイズを取得
-    window_size = Director::getInstance()->getWinSize();
     
     //奈落の底と出す
     auto title_1 = LabelTTF::create("奈", "Arial Rounded MT Bold", 50);
@@ -63,7 +120,7 @@ void Title::crateTitle()
     #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
         auto subtitle = LabelTTF::create("for iOS", "Arial Rounded MT Bold", 25);
     #else
-        auto subtitle = LabelTTF::create("for iOS", "Arial Rounded MT Bold", 25);
+        auto subtitle = LabelTTF::create("for Android", "Arial Rounded MT Bold", 25);
     #endif
     
     //サブタイトル
--- Classes/Title.h
+++ Classes/Title.h
@@ -2,24 +2,41 @@
 #define __TITLE_SCENE_H__
 
 #include "cocos2d.h"
+#include <WebSocket.h>
 
-class Title : public cocos2d::Layer
+USING_NS_CC;
+using namespace network;
+
+class Title : public cocos2d::Layer, WebSocket::Delegate
 {
+    //webSocket
+    WebSocket* websocket;
+    
 public:
     static cocos2d::Scene* createScene();
     virtual bool init();
+    Title();
+    //~Title();
+    CREATE_FUNC(Title);
     
-    //サイズ
+    //Properties
     cocos2d::Size window_size;
+    cocos2d::LabelTTF* status;
     
     //タイトル作成
     void crateTitle();
     
     //外部サーバーとの接続
-    //void loadDataBase();
+    void loadStory();
+    
+    //webSocket
+    virtual void onOpen(WebSocket* ws);
+    virtual void onMessage(WebSocket* ws, const WebSocket::Data& data);
+    virtual void onClose(WebSocket* ws);
+    virtual void onError(WebSocket* ws, const WebSocket::ErrorCode& error);
+    void sendMessage(float dt);
     
     
-    CREATE_FUNC(Title);
 };
 
 #endif // __TITLE_SCENE_H__
